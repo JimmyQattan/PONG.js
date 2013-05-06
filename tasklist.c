85927f9dcb029b8805bda04bad8f6e557fcd3715
umarqattan <u.qattan@gmail.com>
2013-05-05 22:36:07 -0700

    Stufff

--- tasklist.c
+++ tasklist.c
@@ -4,78 +4,37 @@ using namespace std;
 
 Stack::Stack()
 {
-  this->list->head = NULL;
+  this->list = new IntLinkedList();
 }
 Stack::~Stack()
 {
-  this->list->head = NULL;
+  this->list= NULL;
 }
 void Stack::push(int v)
 {
-  IntNode *current = new IntNode(v);
-  current->next = NULL;
-  if(this->list->head == NULL){
-    this->list->head->next = current;
-  }
-  else
-  {
-    current->next = this->list->head;
-    this->list->head = current;
-  }
-
+  this->list->addHead(v);
 }
 bool Stack::isEmpty()
 {
-  return (this->list->head == NULL);
+  return (this->list->removeHead()==0 || this->list->removeTail()==0);
 }
 int Stack::pop()
 {
-  IntNode *current = new IntNode;
-  IntNode *previous = new IntNode;
-  current = this->list->head;
   if(this->isEmpty()){}
   else
   {
-    while(current->next!=NULL)
-    {
-      previous = current;
-      current = current->next;
-    }
-    while(this->list->head->next->next!=NULL)
-    {
-      this->list->head = this->list->head->next;
-    }
-    this->list->head->next = previous;
-    return (current->value);
+    this->list->removeHead();
   }
 }
 int Stack::peek()
 {
-  IntNode *current = this->list->head;
   if(this->isEmpty()){}
-  while(current->next!=NULL)
+  else
   {
-    current = current->next;
+    this->list->peekHead();
   }
-  return (current->value);
 }
 void Stack::printStack(FILE *fp)
 {
-  int i=0;
-  IntNode *current = this->list->head;
-  if(this->isEmpty())
-    printf("No entries in the Stack\n");
-  else
-  {
-    printf("The entires in the Stack are:\n");
-    for(current;current!=NULL;current=current->next)
-    {
-      fprintf(stdout,"%i:",i);
-      current->list->printList(stdout);
-      fprintf(stdout,"\n");
-      i++;
-    }
-  }
+  this->list->printList(fp);
 }
-
-
