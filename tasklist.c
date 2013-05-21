4ab9b2878ea5c6a008b093a77fefdb59daf784f0
umarqattan <u.qattan@gmail.com>
2013-05-21 16:28:12 -0600

    Update tasklist.c

--- tasklist.c
+++ tasklist.c
@@ -1,120 +1,225 @@
-void TaskLinkedList::addTasksFromFile(FILE *fp)
+ * TODO: properly update the tail pointer                                                                                                                
+ * make code more efficient if tail pointer helps                                                                                                        
+ */
+
+void LinkedList::addTail(int v)
 {
-  char buf[50];
-  char *items[5];
-  int i = 0;
-  Task *t1;
-  while(fgets(buf,50,fp)!=NULL)
-  {
-    items[0] = strtok(buf,",\n");
-    items[1] = strtok(NULL,",\n");
-    items[2] = strtok(NULL,",\n");
-    t1 = new Task(items[0],items[2],items[1]);
-    this->addSorted(t1);
-  }
+        // make a new link for the new item                                                                                                              
+        Node *l = new Node();
+        l->value = v;
+        l->next = NULL;
+        // if the list is empty, this is the only one                                                                                                    
+        if (head == NULL)
+        {
+                head = l;
+                tail = l;
+        }
+        // otherwise, we need to go to the back and add the node                                                                                         
+        else
+        {
+
+                // advance l2 so it points to the last link                                                                                              
+          /*I changed l2 to tail, because we're practically creating a new Node that's already been created in the class for us*/
+                for(tail = head;tail->next != NULL; tail = tail->next)
+                        ;
+                // now add this l to the end by hooking l2 to it                                                                                         
+                // Once again, I used tail instead of l2                                                                                                 
+                tail->next = l;
+
+        }
 }
-void TaskLinkedList::addSorted(Task *f)
+
+/* removeHead                                                                                                                                            
+ * remove the first element in a linked list                                                                                                             
+ * TODO: update tail pointer                                                                                                                             
+ */
+
+int LinkedList::removeHead()
 {
-  Node *dummy = new Node();
-   Node *current = new Node();
-  current = this->head;
-  dummy->theTask = f;
-  int comp = f->compare(current->theTask);
-  while(current->next!=NULL){
-  if(comp < 0)
-    this->addHead(f);
-  else if(comp == 0)
-  {
-    dummy = current->next;
-    current->next = dummy;
-  }
-  else
-    current = current->next;
-  }
+        // if the list is empty, return 0                                                                                                                
+        if (head == NULL)
+                return 0;
+        // set a temporary pointer to head                                                                                                               
+        Node *tmp = head;
+        // advance head so it points to the next one                                                                                                     
+        head = head->next;
+        // record the return value before deleting the Link                                                                                              
+        int retval = tmp->value;
+        // delete the Link                                                                                                                               
+        delete tmp;
+        for(tail = head; tail->next != NULL; tail = tail->next)
+          ;
+        // return the value                                                                                                                              
+        return retval;
 }
-__________________________________________________________________________
-//Task.CPP
-int Task::compare(Task *g)
+
+/* removeTail                                                                                                                                            
+ * remove the last element of a linked list                                                                                                              
+ * TODO: update tail pointer                                                                                                                             
+ * make code more efficient if tail pointer helps                                                                                                        
+ */
+int LinkedList::removeTail()
 {
-  Task *newT = g;
-  int date, date2;
-  if(newT->priority < (this->priority))
-    return -1;
-  else if(newT->priority == (this->priority))
+        // if the list is empty, return 0                                                                                                                
+        if (head == NULL)
+                return 0;
+        // if there is only one item, remove it                                                                                                          
+        if (head->next == NULL)
+        {
+                // record the value to return                                                                                                            
+                int retval = head->value;
+                // delete the Link                                                                                                                       
+                delete head;
+                // set head to NULL, indicating an empty list                                                                                            
+                head = NULL;
+                // return the value from that Link                                                                                                       
+                return retval;
+        }
+        // advance to the next to last link                                                                                                              
+
+        for(tail = head;tail->next->next != NULL; tail = tail->next)
+                ;
+
+        // record the return value                                                                                                                       
+        int retval = tail->next->value;
+        // delete the Link                                                                                                                               
+        delete tail->next;
+        // set this node to be pointing to null, removing the Link                                                                                       
+        tail->next = NULL;
+
+        // return the value                                                                                                                              
+        return retval;
+}
+
+/* removeTailRec                                                                                                                                         
+ *                                                                                                                                                       
+ * public method for recursive implementation                                                                                                            
+ * TODO: fill in the code                                                                                                                                
+ */
+int LinkedList::removeTailRec()
+{
+
+        // first do the base cases that do not require recursion                                                                                         
+  if(head == NULL)
     return 0;
-  else
-    return 1;
-
-}
-________________________________________________________________________
-//TaskApp.cpp
-#include <cstdio>
-#include <cstdlib>
-#include <string.h>
-#include "TaskLinkedList.h"
-
-
-
-
-int main(int argc, char *argv[])
-{
-                //File open error                                               
-  //    printf("ERROR: Could not open file %s\n");                              
-  char buf1[50], buf2[100], buf3[100], buf4[50], buf5[50], buf6[50], buf7[50];
-  int p=1,q=0, i, toDelete, length;
-  TaskLinkedList *list = new TaskLinkedList();
-  Task *pTask = new Task;
-  FILE *fp;
-  while(!q)
+  if(head->next == NULL)
   {
-    printf("0) Exit\n1) List Tasks\n2) Add Task\n");                           \
+    int retval = head->value;
+    delete head;
+    head = NULL;
+    return retval;
+  }
 
-    printf("3) Delete Task\n4) Add Task From File\n");                         \
 
-    printf("What would you like to do?\n");
-    fgets(buf1, 50, stdin);                                                    \
-
-    p = atoi(buf1);
-      switch(p)
-      {
-          case 0:
-            q=1;
-            break;
-          case 1:
-            list->printList(fp);
-            break;
-          case 2:
-            printf("Enter task name: ");
-            fgets(buf2,100,stdin);
-            printf("Enter task priority: ");
-            fgets(buf3,100,stdin);
-            printf("Enter task start date: ");
-            fgets(buf4,50,stdin);
-            pTask = new Task(buf2,buf4,buf3);
-            list->addSorted(pTask);
-            break;
-          case 3:
-            list->printList(fp);
-            printf("Enter the index of the task to be deleted? ");
-            fgets(buf6,50,stdin);
-            toDelete = atoi(buf6);
-            list->deleteItem(toDelete);
-            break;
-            //case 4:                                                           
-            //length = list->listLength();                                      
-            //printf("Length of the list : %i\n", length);                      
-            //break;                                                            
-          case 4:
-            printf("Enter the filename ");
-            fgets(buf7,50,stdin);
-            i = strlen(buf7);
-            if((buf7[i-1])=='\n'){buf7[i-1]='\0';}
-            fp = fopen(buf7,"r");
-            if(fp==NULL){printf("ERROR: Could not open file %s\n",buf7);}
-            else{list->addTasksFromFile(fp);}
-              break;
-          default:
-            printf("ERROR: %d: Incorrect menu option\n", p);
-            }
-      }
 }
+
+/* removeTail(Node *)                                                                                                                                    
+ * recursive implementation of removeTail                                                                                                                
+ * TODO: fill in implementation                                                                                                                          
+ */
+
+int LinkedList::removeTailRec(Node *n)
+{
+  // check for the base case(s)                                                                                                                          
+  n = head;
+  if(n == NULL)
+    return 0;
+  if(n->next == NULL)
+  {
+    int retval = head->value;
+    delete n;
+    n = NULL;
+    return retval;
+  }
+  removeTailRec(n->next);
+
+        // else call the recursive method                                                                                                                
+}
+
+void LinkedList::printList(FILE *f)
+{
+        Node *l;
+        fprintf(f,"Printing iteratively forwards: ");
+        fprintf(f,"List: ");
+        for(l = head; l != NULL; l = l->next)
+        {
+                fprintf(f, "%d, ",l->value);
+        }
+        fprintf(f,"\n");
+}
+/* addSorted                                                                                                                                             
+ *                                                                                                                                                       
+ * RECURSIVE public implementation of addSorted                                                                                                          
+* TODO: Fill in implementation                                                                                                                           
+*/
+void LinkedList::addSortedRec(int v)
+{
+  Node *pNode = new Node();
+  pNode->value = v;
+  pNode->next = NULL;
+        // base case(s) outside of recursive call                                                                                                        
+  if(head == NULL)
+  {
+    head = pNode;
+  }
+  if(head->next == NULL)
+  {
+    if(pNode->value < head->value){
+      pNode->next = head;
+      head = pNode;
+      for(tail = head; tail->next!=NULL;tail = tail->next)
+        ;
+    }
+    else{
+      pNode->next = head->next;
+      head->next = pNode;
+      for(tail = head;tail->next!=NULL;tail = tail->next)
+        ;
+    }
+  }
+  head = head->next;
+  addSortedRec(pNode->value);
+}
+
+
+        // call recursive method                                                                                                                         
+
+
+
+
+/* addSorted                                                                                                                                             
+ *                                                                                                                                                       
+ * RECURSIVE private implementation of addSorted                                                                                                         
+*  TODO: fill in code                                                                                                                                    
+*/
+void LinkedList::addSortedRec(Node *n, int v)
+{
+  n->value = v;
+  n->next = NULL;
+
+  // base case(s) outside of recursive call                                                                                                              
+  if(head == NULL)
+    {
+      head = n;
+      tail = n;
+    }
+  //Smaller Case                                                                                                                                         
+  else if(head->next == NULL)
+    {
+      if(n->value < head->value){
+        n->next = head;
+        head = n;
+        tail = n->next;
+      }
+      else{
+        head->next = n;
+        tail = head->next;
+      }
+    }
+  else{}
+
+    }
+
+
+
+
