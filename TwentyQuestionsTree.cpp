0509c662a4f6a551cb8b31084f587830efef5d8c
umarqattan <u.qattan@gmail.com>
2013-06-07 01:27:59 -0600

    Update TwentyQuestionsTree.cpp

--- TwentyQuestionsTree.cpp
+++ TwentyQuestionsTree.cpp
@@ -13,20 +13,20 @@ TwentyQuestionsTree::TwentyQuestionsTree(FILE *fp)
 
     char buf1[512];
     char *questions[6];
-    int i = 0;
+
     while(fgets(buf1,512,fp))
       {
     questions[0] = strtok(buf1,",\n");
-    questions[1] = strtok(NULL,",\n");
-    questions[2] = strtok(NULL,",\n");
-    if(root == NULL) 
-    { 
-        root = new BinaryNode(questions[0]); 
-        root->left = new BinaryNode(questions[1]); 
-        root->right = new BinaryNode(questions[2]); 
-    } 
-    else      
-      insert(root,questions[0],questions[1],questions[2]);
+	questions[1] = strtok(NULL,",\n");
+	questions[2] = strtok(NULL,",\n");
+	if(root == NULL) 
+	{ 
+	  root = new BinaryNode(questions[0]);
+	  root->left = new BinaryNode(questions[1]); 
+	  root->right = new BinaryNode(questions[2]); 
+	} 
+	else	  
+	  insert(root,questions[0],questions[1],questions[2]);
       }
        
 }
@@ -39,6 +39,15 @@ TwentyQuestionsTree::TwentyQuestionsTree()
 
 bool TwentyQuestionsTree::insert(BinaryNode *r,char *parent, char *left, char *right)
 {
+  if(r && r->left == NULL && r->right == NULL)
+  {
+    delete r->question;
+    r->question = new char[strlen(parent)+1];
+    strcpy(r->question,parent);
+    r->left = new BinaryNode(left);
+    r->right = new BinaryNode(right);
+    return true;
+  }
   if(strcmp(r->question,parent)==0)
     {
       r->left = new BinaryNode(left);
@@ -48,9 +57,9 @@ bool TwentyQuestionsTree::insert(BinaryNode *r,char *parent, char *left, char *r
   else
     {
       if(r->left != NULL)
-    if(insert(r->left,parent,left,right)) return true;
+	if(insert(r->left,parent,left,right)) return true;
       if(r->right != NULL)
-    if(insert(r->right,parent,left,right)) return true;
+	if(insert(r->right,parent,left,right)) return true;
       return false;
     }
 }
@@ -62,72 +71,31 @@ void TwentyQuestionsTree::insert(char* parent, char* left, char* right)
 bool TwentyQuestionsTree::modifyAndInsert(BinaryNode *n,char *parent, char *left, char *right)
 {
   
-  if(strcmp(n->question,left)==0 || strcmp(n->question,right)==0)
-  {
-      n->question = parent;
-      n->left = new BinaryNode(left);
-      n->right = new BinaryNode(right);
-      return true;
-  }
+    if(strcmp(n->question,left)==0 || strcmp(n->question,right)==0)
+      {
+	delete n->question;
+	n->question = new char[strlen(parent)+1];
+	strcpy(n->question,parent);
+	n->left = new BinaryNode(left);
+	n->right = new BinaryNode(right);
+	return true;
+      }
   else
   {
-      if(n->left!=NULL)
-    if(modifyAndInsert(n->left,parent,left,right))
-      return true;
-      else if(n->right!=NULL)
-    if(modifyAndInsert(n->right,parent,left,right))
-      return true;
-      else
-    return false;
+    if(n->left!=NULL)
+    {
+      if(modifyAndInsert(n->left,parent,left,right))
+	return true;
+    }
+    else if(n->right!=NULL)
+    {
+      if(modifyAndInsert(n->right,parent,left,right))
+	return true;
+    }
+    else
+      return false;
   }
-}
-void TwentyQuestionsTree::modifyAndInsert(char *parent, char *left, char *right)
-{
-  modifyAndInsert(root,parent,left,right);
-}
-
-
-void TwentyQuestionsTree::reset()
-{
-  iterator = root;
-}
-
-
-char *TwentyQuestionsTree::currentQuestion()
-{
-  if(iterator == NULL)
-    return NULL;
-  else
-    return iterator->question;
-}
-
-
-
-void TwentyQuestionsTree::recordAnswer(int answer)
-{
-  if(answer == 0)
-    iterator = iterator->left;
-  else if(answer == 1)
-    iterator = iterator->right;
-  else
-    return;
-
-}
-
-
-void TwentyQuestionsTree::storeTree(BinaryNode *n,FILE *fp)
-{
-  if(n->left == NULL)
-    return;
-  else
-  {
-    fprintf(fp,"%s,%s,%s\n",n->question,n->left->question,n->right->question);
-    storeTree(n->left,fp);
-    storeTree(n->right,fp);
-  }
-}
-
-void TwentyQuestionsTree::storeTree(FILE *fp)
-{
-  storeTree(root, fp);
+  
+    
+ 
 }
